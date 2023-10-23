4bb96c22ef1c96a654f33b85837ecdc54d4c7794
Jane Tran <ttran161@ucsc.edu>
2023-10-23 12:09:14 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,54 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const context = canvas.getContext("2d")!;
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+canvas.addEventListener("mousedown", (e: MouseEvent) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e: MouseEvent) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+window.addEventListener("mouseup", (e: MouseEvent) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(
+  context: CanvasRenderingContext2D,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 2;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
+
+const clearButton = document.getElementById("clearButton") as HTMLButtonElement;
+clearButton.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
